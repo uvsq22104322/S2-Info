f9597a82bd37d473d3117f82c69225a2870f4228
Cyriac THIBAUDEAU <cyriac.thibaudeau@ens.uvsq.fr>
2022-04-16 00:55:23 +0200

    essai fonction clear/callback

--- Projet/Projet Boulier.py
+++ Projet/Projet Boulier.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+#Variables Global
 DIM_BOULIER = (17,9)
 WIDTH = 1300
 HEIGHT = 700
@@ -7,13 +8,14 @@ ecart_width = WIDTH/DIM_BOULIER[0]
 ecart_height = HEIGHT/DIM_BOULIER[1]
 boules = []
 
+#Obtenir les coordonnées et l'ID des boules 
 def clic(event):
     x, y = event.x, event.y
     print("Clic en",x,",",y)
     print(canvas1.find_closest(x,y)," est l'objet cliqué.")
 
 
-
+#Mise en place du Canvas
 racine=tk.Tk()
 racine.title("Projet Boulier")
 
@@ -22,30 +24,42 @@ canvas1 = tk.Canvas(racine, width=WIDTH, height=HEIGHT, bg="black", borderwidth=
 #Création des boules
 
 def Start():
+    global ID_boules
     for x in range(DIM_BOULIER[0]):
         for y in range(DIM_BOULIER[1]):
             if y == 1 or y == 3:
-                canvas1.create_oval(x*ecart_width,y*ecart_height,x*ecart_width+ecart_width,y*ecart_height+ecart_height,fill="gray",tags="boules")
+                ID_boules = canvas1.create_oval(x*ecart_width,y*ecart_height,x*ecart_width+ecart_width,y*ecart_height+ecart_height,fill="gray",tags="boules")
             elif y == 2:
                 if y == 2 and x%3 == 1 :
-                    canvas1.create_oval(x*ecart_width,y*ecart_height,x*ecart_width+ecart_width,y*ecart_height+ecart_height,fill="white")
+                    ID_boules = canvas1.create_oval(x*ecart_width,y*ecart_height,x*ecart_width+ecart_width,y*ecart_height+ecart_height,fill="white", tag="boules")
                 else :
-                    canvas1.create_oval(x*ecart_width,y*ecart_height,x*ecart_width+ecart_width,y*ecart_height+ecart_height,fill="black")
+                    ID_boules = canvas1.create_oval(x*ecart_width,y*ecart_height,x*ecart_width+ecart_width,y*ecart_height+ecart_height,fill="black")
             else:
-                canvas1.create_oval(x*ecart_width,y*ecart_height,x*ecart_width+ecart_width,y*ecart_height+ecart_height,fill="red",tags="boules")
-#        boules = boules.append()
+                ID_boules = canvas1.create_oval(x*ecart_width,y*ecart_height,x*ecart_width+ecart_width,y*ecart_height+ecart_height,fill="red",tags="boules")
+            boules.append(ID_boules)
+    print(boules) #vérif ID_boules
 
-#def Clear():
-#    list = racine.grid()
-#    for l in list:
-#        l.destroy()
+#Supression des boules (pour l'instant, ne vide uniquement la liste boules)
+def Clear():
+    #for i in range(ID_boules):
+    #    boules[i] = 0
+    #    i += 1
+    boules.clear()
+    print(boules) #Vérif ID_boules
 
+def callback():
+    canvas1.itemconfig(ID_boules,fill='red')
+    print(boules)
+
+#Mise en place des boutons 
 button1 = tk.Button(racine, text="start", bg="red", command = Start)
-#button2 = tk.Canvas(racine, text="Clear", bg="red") #command = Clear
+button2 = tk.Button(racine, text="clear", bg="red", command = callback)
 
+#Positionnement des éléments
 canvas1.grid(column=1, row=1, rowspan=9, columnspan=17)
 button1.grid(column=0, row=0)
-#button2.grid(column=0, row=1)
+button2.grid(column=0, row=1)
 canvas1.tag_bind("boules","<Button-1>",clic)
 
+#Activation de l'interface graphique
 racine.mainloop()
